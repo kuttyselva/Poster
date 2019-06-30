2c08bbd763cf6f90c8306bf9334ce5888cf36ea2
kutty selva <kuttyselva57@gmail.com>
2019-06-30 17:58:37 +0530

    d

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const app=express();
 const path=require('path');
-app.use(express.static(__dirname + '/src'));
-app.listen(process.env.PORT || 4200);
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
 //pathlocation
 app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname + '/src/index.html'));
